bb686ce1a120d1f88a6ceaa6275cc5fa3d947a59
Stanley Brenner <stanleybrenner@gmail.com>
2025-10-02 00:19:02 -0300

    first commit

---
+++ jest.config.mjs
@@ -0,0 +1,16 @@
+﻿import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config = {
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  testMatch: ["**/__tests__/**/*.test.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+};
+
+export default createJestConfig(config);
